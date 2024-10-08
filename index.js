007bb47c9c84dce4fc93d276277233b063b849b2
Gstechnology18 <125030405+Gstechnology18@users.noreply.github.com>
2024-10-08 10:00:52 -0300

    Versão 1.0.0

--- index.js
+++ index.js
@@ -6,8 +6,24 @@ const app = express()
 app.engine("handlebars", exphbs.engine())
 app.set('view engine', 'handlebars') //colocar uma variavel que armazena temporariamente
 
+
+app.get('/peixe', (req, res) => {
+    res.render('peixe')
+})
+
 app.get('/', (req, res) => {//associa uma prp
-    res.render('acai')
+    const user = {
+        name: 'Guilherme',
+        age: 19,
+        email: 'guiolvsan@gmail.com'
+        
+    }
+    const auth = false
+
+    const approved = true
+
+    res.render('acai', {user: user, auth, approved})
+
 
 })
 
